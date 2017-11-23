d9c7a3615ae5ca906a581226670de6cf710a2ec1
choxnox <chox.nox@gmail.com>
2017-11-23 17:15:18 +0100

    Version 1.0.1

--- test/validators.callback.js
+++ test/validators.callback.js
@@ -15,7 +15,7 @@ describe("Validators", function() {
             };
         });
 
-        it("should allow callback to be a function", function(done) {
+        it("should allow callback to be a sync function", function(done) {
             var Callback = new Validate.Callback({
                 callback: function(value, options) {
                     return true;
@@ -28,6 +28,55 @@ describe("Validators", function() {
             Callback.isValid(str, {}, function(err, value) {
                 expect(err).to.equal(null);
                 expect(value).to.equal(str);
+                done();
+            });
+        });
+
+        it("should allow callback to be an async function", function(done) {
+            var Callback = new Validate.Callback({
+                callback: function(value, options, callback) {
+                    callback(null);
+                },
+                messages: messages
+            });
+
+            var str = "abc";
+
+            Callback.isValid(str, {}, function(err, value) {
+                expect(err).to.equal(null);
+                expect(value).to.equal(str);
+                done();
+            });
+        });
+
+        it("should return 'invalid_value' when sync callback validation fails", function(done) {
+            var Callback = new Validate.Callback({
+                callback: function(value, options) {
+                    return false;
+                },
+                messages: messages
+            });
+
+            var str = "abc";
+
+            Callback.isValid(str, {}, function(err, value) {
+                expect(err).to.equal("invalid_value");
+                done();
+            });
+        });
+
+        it("should return 'invalid_value' when async callback validation fails", function(done) {
+            var Callback = new Validate.Callback({
+                callback: function(value, options, callback) {
+                    callback(true);
+                },
+                messages: messages
+            });
+
+            var str = "abc";
+
+            Callback.isValid(str, {}, function(err, value) {
+                expect(err).to.equal("invalid_value");
                 done();
             });
         });
