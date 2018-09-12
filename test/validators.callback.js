53a3f71a97a41bac8bfdeff885a07d2c944e85df
choxnox <chox.nox@gmail.com>
2018-09-12 16:25:10 +0200

    Removed the whole lodash package and used only what's necessary from lodash module

--- test/validators.callback.js
+++ test/validators.callback.js
@@ -1,3 +1,10 @@
+/**
+ * @Date:   2018-01-03T16:24:09+01:00
+ * @Last modified time: 2018-08-19T16:11:29+02:00
+ */
+
+
+
 var chai = require("chai");
 
 var expect = chai.expect;
@@ -17,7 +24,7 @@ describe("Validators", function() {
 
         it("should allow callback to be a sync function", function(done) {
             var Callback = new Validate.Callback({
-                callback: function(value, options) {
+                callback: function(value, context, options) {
                     return true;
                 },
                 messages: messages
@@ -34,7 +41,7 @@ describe("Validators", function() {
 
         it("should allow callback to be an async function", function(done) {
             var Callback = new Validate.Callback({
-                callback: function(value, options, callback) {
+                callback: function(value, context, options, callback) {
                     callback(null);
                 },
                 messages: messages
@@ -51,7 +58,7 @@ describe("Validators", function() {
 
         it("should return 'invalid_value' when sync callback validation fails", function(done) {
             var Callback = new Validate.Callback({
-                callback: function(value, options) {
+                callback: function(value, context, options) {
                     return false;
                 },
                 messages: messages
@@ -67,7 +74,7 @@ describe("Validators", function() {
 
         it("should return 'invalid_value' when async callback validation fails", function(done) {
             var Callback = new Validate.Callback({
-                callback: function(value, options, callback) {
+                callback: function(value, context, options, callback) {
                     callback(true);
                 },
                 messages: messages
